a9b2023165e9ac766afc781e848ca731821cfa92
5a4d4 <122555188+5a4d4@users.noreply.github.com>
2024-09-21 23:43:00 -0500

    Add files via upload

---
+++ practice.cs
@@ -0,0 +1,16 @@
+//I am going to start with the practice//
+//The first part with text//
+console.writeline ("Hello World");
+console.writeline ("There will not be a newline after this");
+console.writeline ("What is your favorite color?");
+string color = console.readline ();
+
+//The second part with numbers//
+if (x > y)
+{
+    console.writeline ("greater");
+}
+
+//More practice with text//
+string school = "BYU-Idaho";
+console.writeline ($"I am studying at {school}");
